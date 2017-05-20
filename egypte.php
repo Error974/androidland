3b3e2053032a06e002c40818b059cdb9ceec6566
Etheve Samuel <etheve.samuel@gmail.com>
2017-05-21 02:59:15 +0400

    egyte

---
+++ egypte.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>AndroidLand - Vivez de belles histoires</title>
+
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Font Awesome CSS -->
+		<link href="css/font-awesome/font-awesome.min.css" rel="stylesheet">
+
+		<!-- Simple Line Icons -->
+		<link href="css/simple-line-icons/simple-line-icons.css" rel="stylesheet">
+
+		<!-- google font -->
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+
+		<!-- owl-carousel -->
+		<link href="plugins/owl-carousel/owl.carousel.css" rel="stylesheet">
+		<link href="plugins/owl-carousel/owl.theme.css" rel="stylesheet">
+
+		<!-- magnific-popup -->
+		<link href="plugins/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+		<!-- animate -->
+		<link href="css/animate/animate.css" rel="stylesheet">
+
+		<!-- style -->
+		<link href="css/style.css" rel="stylesheet">
+		<link href="css/custom.css" rel="stylesheet">		<link rel="stylesheet" href="css/style-detail.css">
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body class="slider-background">
+
+		<!-- Preloader 	-->
+		<div id="preloader">
+			<div id="status">&nbsp;</div>
+		</div>
+		<!-- ./Preloader -->
+
+		<!-- pattern -->
+		<div id="bg_pattern"></div>
+		<!-- ./pattern -->
+
+		<!-- scrollToTop -->
+		<a href="slider-background.html#" class="scrollToTop">
+			<i class="fa fa-angle-up fa-2x"></i>
+		</a>
+		<!-- ./scrollToTop -->
+
+
+		<!-- header -->
+		<header id="header">
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+			  <div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse-navbar" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				  <a class="navbar-brand" href="index.html">						<img src="img\logo1.png" alt="image" class="img-responsive">
+						<!-- <i class="fa fa-cog logo"></i> -->
+				  </a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="collapse-navbar">
+
+				  <ul class="nav navbar-nav navbar-right">
+						<li><a data-scroll href="index.html#concept">Concept</a></li>
+						<li><a data-scroll href="index.html#nos_univers">Nos univers</a></li>
+						<li><a data-scroll href="index.html#univers_disponible">Aperçus</a></li>
+						<li><a data-scroll href="index.html#team">Androides Stars</a></li>
+						<li><a data-scroll href="index.html#news">Nouveautés</a></li>
+						<li><a data-scroll href="index.html#pricing">Billets d'entrée</a></li>
+						<li><a data-scroll href="index.html#testimonial">Avis</a></li>
+						<li><a data-scroll href="index.html#contact">Contact</a></li>
+				  </ul>
+				</div><!-- /.navbar-collapse -->
+			  </div><!-- /.container -->
+			</nav>
+		</header>
+		<!-- ./header -->
+				<!-- pricing -->
+
+				<section id="pricing" class="section wow fadeInUp">
+
+					<div class="container">
+
+						<div class="section-heading">
+
+							<h2>Tarifs</h2>
+
+							<div class="separator"></div>
+
+						</div>
+
+
+						<div class="row">
+
+							<div class="col-md-4">
+
+								<div class="price-box2">
+
+									<h2>Tarif simple</h2>
+
+									<div class="separator"></div>
+
+									<p>*Valable pour une seule et unique personne</p>
+
+									<div class="price">
+
+										29<span>€</span>
+
+										<div class="month">/ jour</div>
+
+									</div>
+
+									<ul>
+
+										<li>Accès restaurant, attractions</li>
+
+										<li>Accès piscine gratuite</li>
+
+										<li>Accès Scénarios/Animations</li>
+
+									</ul>
+
+									<a href="slider-background.html#" class="btn btn-dark btn-lg btn-block">Sélectionner cette offre</a>
+
+								</div>
+
+							</div>
+
+							<div class="col-md-4">
+
+								<div class="price-box2">
+
+									<h2>Tarif Groupe/Famille</h2>
+
+									<div class="separator"></div>
+
+									<p>*Valable pour un minimum de 3 personnes</p>
+
+									<div class="price">
+
+										20<span>€</span>
+
+										<div class="month">/ jour par personne</div>
+
+									</div>
+
+									<ul>
+
+										<li>Accès restaurant, attractions</li>
+
+										<li>Accès piscine gratuite</li>
+
+										<li>Accès Scénarios/Animations</li>
+
+									</ul>
+
+									<a href="slider-background.html#" class="btn btn-dark btn-lg btn-block">Sélectionner cette offre</a>
+
+								</div>
+
+							</div>
+
+							<div class="col-md-4">
+
+								<div class="price-box2">
+
+									<h2>Tarif Premium</h2>
+
+									<div class="separator"></div>
+
+									<p>*Valable pour les pocesseur de la carte premium</p>
+
+									<div class="price">
+
+										15<span>€</span>
+
+										<div class="month">/ jour par personne</div>
+
+									</div>
+
+									<ul>
+										<li>Accès restaurant, attractions</li>
+										<li>Accès piscine gratuite</li>
+										<li>Accès Scénarios/Animations</li>
+									</ul>
+									<a href="slider-background.html#" class="btn btn-dark btn-lg btn-block">En savoir plus</a>
+								</div>
+							</div>
+							<p>*Tarifs dégressif pour association et comité entreprise, merci de prendre contact au 0262 123 456 ou mail contact@androidland.com</p>
+						</div><!-- ./row -->
+					</div>
+				</section>
+
+				<!-- ./pricing -->
+
+
+		<div class="container">        <div id="myCarousel" class="carousel slide" data-ride="carousel">          <!-- Wrapper for slides -->          <div class="carousel-inner">            <div class="item active">              <img src="http://placehold.it/760x400/cccccc/ffffff">               <div class="carousel-caption">                <h4><a href="#">Lorem ipsum dolor sit amet consetetur sadipscing</a></h4>                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>              </div>            </div><!-- End Item -->             <div class="item">              <img src="http://placehold.it/760x400/999999/cccccc">               <div class="carousel-caption">                <h4><a href="#">consetetur sadipscing elitr, sed diam nonumy eirmod</a></h4>                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>              </div>            </div><!-- End Item -->            <div class="item">              <img src="http://placehold.it/760x400/dddddd/333333">               <div class="carousel-caption">                <h4><a href="#">tempor invidunt ut labore et dolore</a></h4>                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>              </div>            </div><!-- End Item -->            <div class="item">              <img src="http://placehold.it/760x400/999999/cccccc">               <div class="carousel-caption">                <h4><a href="#">magna aliquyam erat, sed diam voluptua</a></h4>                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>              </div>            </div><!-- End Item -->            <div class="item">              <img src="http://placehold.it/760x400/dddddd/333333">               <div class="carousel-caption">                <h4><a href="#">tempor invidunt ut labore et dolore magna aliquyam erat</a></h4>                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>              </div>            </div><!-- End Item -->          </div><!-- End Carousel Inner -->        <ul class="list-group col-sm-4">          <li data-target="#myCarousel" data-slide-to="0" class="list-group-item active"><h4>Lorem ipsum dolor sit amet consetetur sadipscing</h4></li>          <li data-target="#myCarousel" data-slide-to="1" class="list-group-item"><h4>consetetur sadipscing elitr, sed diam nonumy eirmod</h4></li>          <li data-target="#myCarousel" data-slide-to="2" class="list-group-item"><h4>tempor invidunt ut labore et dolore</h4></li>          <li data-target="#myCarousel" data-slide-to="3" class="list-group-item"><h4>magna aliquyam erat, sed diam voluptua</h4></li>          <li data-target="#myCarousel" data-slide-to="4" class="list-group-item"><h4>tempor invidunt ut labore et dolore magna aliquyam erat</h4></li>        </ul>          <!-- Controls -->          <div class="carousel-controls">              <a class="left carousel-control" href="#myCarousel" data-slide="prev">                <span class="glyphicon glyphicon-chevron-left"></span>              </a>              <a class="right carousel-control" href="#myCarousel" data-slide="next">                <span class="glyphicon glyphicon-chevron-right"></span>              </a>          </div>        </div><!-- End Carousel -->    </div>
+		<!-- news -->
+		<section id="news" class="section wow fadeInUp">
+			<div class="container">
+
+				<div class="section-heading">
+					<h2>Nouveautés</h2>
+					<div class="separator"></div>
+				</div>
+
+				<!-- blog-carousel2 -->
+				<div id="blog-carousel2" class="owl-carousel">
+					<div class="item">
+						<div class="blog-box2">
+							<figure>
+								<img src="img/photos/news/new1.jpg" alt="image" class="img-responsive">
+								<ul>
+									<li><i class="fa fa-calendar"></i> <span>3 Janvier 2017</span></li>
+								</ul>
+							</figure>
+							<h4>Future mise à jour</h4>
+							<p> La version 3.14 tant attendue de nos androïdes arrivent ! Cette mise à jour apporte son lot d'améliorations habituel, accruant encore plus les réactions de nos hôtes.</p>
+						</div>
+					</div>
+					<div class="item">
+						<div class="blog-box2">
+							<figure>
+								<img src="img/photos/news/img1.jpg" alt="image" class="img-responsive">
+								<ul>
+									<li><i class="fa fa-calendar"></i> <span>21 Décembre 2016</span></li>
+								</ul>
+							</figure>
+							<h4>Le parc s'enneige</h4>
+							<p> Quoi de mieux qu'une épaisse couche de neige pour rappeller Noêl ? Cette année, vore parc ajoute une couche de magie supplémentaire en revêtant ce manteau tout blanc.</p>
+							<!-- <a href="blog-single.html" class="link">Read more...</a> -->
+						</div>
+					</div>
+					<div class="item">
+						<div class="blog-box2">
+							<figure>
+								<img src="img/photos/news/img3.png" alt="image" class="img-responsive">
+								<ul>
+									<li><i class="fa fa-calendar"></i> <span>14 Novembre 2016</span></li>
+								</ul>
+							</figure>
+							<h4>Fermeture exceptionnelle</h4>
+							<p> En ce jour du 14 Novembre 2016, le parc sera exceptionnellement fermé pour des travaux de maintenance sur l'ensemble des secteurs des différentes zones. Merci de votre compréhension.</p>
+							<!-- <a href="blog-single.html" class="link">Read more...</a> -->
+						</div>
+					</div>
+					<div class="item">
+						<div class="blog-box2">
+							<figure>
+								<img src="img/photos/news/img4.jpg" alt="image" class="img-responsive">
+								<ul>
+									<li><i class="fa fa-calendar"></i> <span>20 Octobre 2016</span></li>
+								</ul>
+							</figure>
+							<h4>Des zombies dans le parc ??</h4>
+							<p> Halloween arrive à grand pas... Mais ? Qu'est ce que ...?! Des zombies ? Fuyez, ou, si vous vous en sentez capable, venez cotoyez nos androïdes version zombie dans un parc frissonant !</p>
+							<!-- <a href="blog-single.html" class="link">Read more...</a> -->
+						</div>
+					</div>
+					<div class="item">
+						<div class="blog-box2">
+							<figure>
+								<img src="img/photos/news/img5.jpg" alt="image" class="img-responsive">
+								<ul>
+									<li><i class="fa fa-calendar"></i> <span>2 Octobre 2016</span></li>
+								</ul>
+							</figure>
+							<h4>Prélude évènementiel</h4>
+							<p> Quelle est cette forme ...? Attendez que je m'approche ...</p>
+						</div>
+					</div>
+
+
+				</div>
+				<!-- ./blog-carousel2 -->
+
+			</div>
+
+		</section>
+		<!-- ./news -->
+
+
+
+		<!-- footer -->
+		<footer id="footer">
+			<div class="container">
+				<ul class="list-inline">
+					<li><a href="https://www.facebook.com/Error-974-Webcup-R%C3%A9union-2017-231611863992308/"><i class="fa fa-facebook"></i></a></li>
+				</ul>
+				<div class="copyright">&copy; 2017 AndroidLand | <a href="https://www.facebook.com/Error-974-Webcup-R%C3%A9union-2017-231611863992308/" target="_blank">by ERROR974</a></div>
+			</div>
+		</footer>
+		<!-- ./footer -->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="js/jquery-1.11.3.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+
+		<!-- smooth-scroll -->
+		<script src="plugins/smooth-scroll/smooth-scroll.js"></script>
+
+		<!-- backstretch -->
+		<script src="plugins/backstretch/backstretch.min.js"></script>
+
+		<!-- owl-carousel -->
+		<script src="plugins/owl-carousel/owl.carousel.js"></script>
+
+		<!-- wow -->
+		<script src="plugins/wow/wow.js"></script>
+
+		<!-- typed -->
+		<script src="plugins/typed/typed.min.js"></script>
+
+		<!-- magnific-popup -->
+		<script src="plugins/magnific-popup/jquery.magnific-popup.js"></script>
+
+		<!-- jqBootstrapValidation -->
+		<script src="plugins/jqBootstrapValidation/jqBootstrapValidation.js"></script>
+
+		<!-- switcher -->
+		<script type="text/javascript" src="switcher/switcher.js"></script>
+
+		<!-- main js -->
+		<script src="js/main.js"></script>		<script src="js/jsdetail.js" charset="utf-8"></script>
+
+	</body>
+</html>
